2ea6bd2dc11a785c7d382dfbe10311519322ed9b
George Spanos <gespa1101@9gmail.com>
2022-06-12 17:40:27 +0300

    finilize precommit hooks

--- libs/rsm/src/lib/rsm.ts
+++ libs/rsm/src/lib/rsm.ts
@@ -1,3 +1,7 @@
 export function rsm(): string {
+  console.log(
+    'fasolakiafasasdfasdasdasdolakiafasolakiafasolakiafasolakiafasolakia',
+  );
+  console.log('asdasdasdddddddddddddddd');
   return 'rsm';
 }
