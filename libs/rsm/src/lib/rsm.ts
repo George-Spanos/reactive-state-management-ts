9916ef5a03c6399a0d6f27202e84b995932ff037
George Spanos <gespa1101@9gmail.com>
2022-06-12 17:42:23 +0300

    cleaning starter library files

--- libs/rsm/src/lib/rsm.ts
+++
@@ -1,7 +0,0 @@
-export function rsm(): string {
-  console.log(
-    'fasolakiafasasdfasdasdasdolakiafasolakiafasolakiafasolakiafasolakia',
-  );
-  console.log('asdasdasdddddddddddddddd');
-  return 'rsm';
-}
